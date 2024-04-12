f5819b7ede5b62fb77163c4c9c241bff112efd69
Lance <dogmandevices@gmail.com>
2024-04-12 01:02:44 -0500

    returning to calling nth number recursively

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,33 +1,10 @@
 # frozen_string_literal: true
 
-class Fibs
-  attr_accessor :len, :seq, :acc
+def fibo(n)
+  n < 2 ? n : fibo(n - 1) + fibo(n - 2)
+end 
 
-  def initialize(len)
-    @len = len
-    @seq = []
-    @acc = 0
-  end
-
-  def fibo
-    if @acc < 2
-      @seq.unshift(@acc)
-      @acc += 1
-    else
-      @acc = fibo((@acc - 1)) + fibo((@acc - 2))
-      @seq.unshift(@acc)
-    end
-    if @seq.length == len
-      @seq
-    end
-  end
-end
 
 test = [0, 1, 1, 2, 3, 5, 8, 13]
 # p test
-fibs = Fibs.new(7)
-i = 0
-while i < 7
-  p fibs.fibo
-  i += 1
-end
+p fibo(7)
