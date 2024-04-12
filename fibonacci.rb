4b263a4b150d9f47c3546316e35125d61c9a19a0
Lance <dogmandevices@gmail.com>
2024-04-12 01:01:06 -0500

    trying to use a class to build an array of numbers, having strong second thoughts about it

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,14 +1,33 @@
 # frozen_string_literal: true
 
-def fibo(n)
-  if n < 2
-   p n
-  else
-    p fibo(n - 1) + fibo(n - 2)
+class Fibs
+  attr_accessor :len, :seq, :acc
+
+  def initialize(len)
+    @len = len
+    @seq = []
+    @acc = 0
+  end
+
+  def fibo
+    if @acc < 2
+      @seq.unshift(@acc)
+      @acc += 1
+    else
+      @acc = fibo((@acc - 1)) + fibo((@acc - 2))
+      @seq.unshift(@acc)
+    end
+    if @seq.length == len
+      @seq
+    end
   end
 end
 
-
 test = [0, 1, 1, 2, 3, 5, 8, 13]
 # p test
-fibo(7)
+fibs = Fibs.new(7)
+i = 0
+while i < 7
+  p fibs.fibo
+  i += 1
+end
